fc4631c442ebb70d04a6dcd01358098f666be39a
Ananya Ravi <ec21b1004@gmail.com>
2022-12-15 22:23:59 +0530

    1st commit

---
+++ pnp_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/ananya/MARS/Pick-and-Place-Manipulator/pnp_ws/src
+# Build directory: /home/ananya/MARS/Pick-and-Place-Manipulator/pnp_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("rob_description_pkg")
---
+++ pnp_ws/build/rob_description_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/ananya/MARS/Pick-and-Place-Manipulator/pnp_ws/src/rob_description_pkg
+# Build directory: /home/ananya/MARS/Pick-and-Place-Manipulator/pnp_ws/build/rob_description_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
